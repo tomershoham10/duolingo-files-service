03d71a0b22ba69b0e96fa9dda49ea34ef252d6d7
tomershoham10 <tomershoham10@gmail.com>
2024-01-11 09:49:49 +0200

    fix: metadata type fields

--- src/files/model.ts
+++ src/files/model.ts
@@ -10,15 +10,16 @@ enum SonarSystem {
 }
 
 export interface FileMetadata {
-  recordLength: number;
-  difficultyLevel: number;
-  targetsIds_list: string[];
+  sonograms_ids:string[];
+  record_length: number;
+  difficulty_level: number;
+  targets_ids_list: string[];
   operation: string;
   source: string;
   is_in_italy: boolean;
   transmition: Transmissions;
   channels_number: number;
-  sonar_syster: SonarSystem;
+  sonar_system: SonarSystem;
   is_backround_vessels: boolean;
   aux: boolean;
 }
